49e9ffe879ce9719dcb2cbed06a3fee8489ff85e
MylesSHarris <69069959+MylesSHarris@users.noreply.github.com>
2020-10-06 09:19:16 -0700

    commit

---
+++ Part1/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Part1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        double annualInterest = 0.0639;
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double monthlyPayment = Double.Parse(monthlyPaymentText.Text);
+            double outstandingBalance = Double.Parse(outstandingBalanceText.Text);
+
+            double monthlyInterest = annualInterest / 12;
+            double interestPayment = monthlyInterest * outstandingBalance;
+
+            double principalPayment = monthlyPayment - interestPayment;
+
+            MessageBox.Show("You must pay $" + Math.Round(interestPayment, 2) + " to interest and $" + Math.Round(principalPayment, 2) + " to principal.");
+        }
+    }
+}
---
+++ Part2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter price of item(from 25 cents to a dollar, in 5 - cent increments): ");
+
+            int price = Int32.Parse(Console.ReadLine());
+            int extra = 100 - price;
+
+            int quartersReturned = extra / 25;
+            extra -= quartersReturned * 25;
+
+            int dimesReturned = extra / 10;
+            extra -= dimesReturned * 10;
+
+            int nicklesReturned = extra / 5;
+
+            Console.WriteLine($"You bought an item for ${price / 100.0} and gave me a dollar.");
+            Console.WriteLine($"Your change is:");
+            Console.WriteLine($"{quartersReturned} quarters, {dimesReturned} dimes, and {nicklesReturned} nickles.");
+        }
+    }
+}
